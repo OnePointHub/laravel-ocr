c07d60ccf326a5b38b7221ba63da1e9486db96da
Vasileios Ntoufoudis <info@ntoufoudis.com>
2024-10-06 13:37:58 +0300

    Install Tesseract in Workflow

--- tests/OcrTest.php
+++ tests/OcrTest.php
@@ -2,7 +2,7 @@
 
 use Ntoufoudis\LaravelOcr\Facades\Ocr;
 
-it('can get the image', function () {
+it('can get the text from an image', function () {
     $imagePath = __DIR__ . '/../resources/sampleImages/1.jpg';
     $text = Ocr::scan($imagePath);
     $this->assertTrue(str_contains($text, "Example"));
